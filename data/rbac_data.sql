f52276f14eb132acf5a3232f29e60f2912ab0ee4
xa2kx <schulal2@b-tu.de>
2015-10-30 15:03:46 +0100

    updated rbac data

--- data/rbac_data.sql
+++ data/rbac_data.sql
@@ -23,12 +23,14 @@ INSERT INTO "role" VALUES ('c0057bdd-b3ba-4e80-afb6-3cf5cb51ebce','root','super 
 INSERT INTO "role" VALUES ('aef02838-4b92-44e4-bf6a-dc8655b6844b','sysadmin','Der Systemadministrator soll das Nutzermanagement und die Projektverwaltung durchführen können. Das schließt die Zuordnung von Nutzern zu Projekten und die allgemeine Konfiguration von OpenInfRA mit ein.');
 INSERT INTO "role" VALUES ('fe45a9e1-1fe6-4b9b-9c92-6f71559c5771','syseditor','Der Systembearbeiter soll Inhalte der Systemdatenbank lesen und bearbeiten können.');
 INSERT INTO "role" VALUES ('9b3e5f1b-c720-4242-a9d5-c243b68fc301','sysguest','Der Systemgast soll ausschließlich lesenden Zugriff auf die Inhalte der Systemdatenbank haben.');
+INSERT INTO "role" VALUES ('e9c3921f-ccaa-4ad7-abfc-e8881f2440c5','fileuploader','Eine Rolle zum Hochladen von Bildern.');
 
 INSERT INTO "permission" VALUES ('97e4d76e-9e40-43a1-b1f5-e9e1e7ca147e','permission for root','*:*:*');
 INSERT INTO "permission" VALUES ('8e618e6b-7f07-471e-95b4-9af66c6acfe8','permission for reading system database','/system:r');
 INSERT INTO "permission" VALUES ('1fbfd6eb-4b48-4819-829d-59909caf4b7c','permission for reading and writing system database','/system:r,w');
 INSERT INTO "permission" VALUES ('5fdf7895-3717-476d-8373-fc56a60350aa','permission for reading and writing rbac database','/rbac:r,w');
 INSERT INTO "permission" VALUES ('c906813d-2268-4f94-9dfb-a467e4340cd5','permission for creating new project schemas','/projects:r,w');
+INSERT INTO "permission" VALUES ('86353fab-0b70-43f8-84bc-1c5d81a94f9e','permission for uploading files','/files:r,w');
 
 -- root
 INSERT INTO "role_permissions" VALUES ('2a5d3193-1bef-4bec-9fee-9142806af191','c0057bdd-b3ba-4e80-afb6-3cf5cb51ebce','97e4d76e-9e40-43a1-b1f5-e9e1e7ca147e');
@@ -39,6 +41,8 @@ INSERT INTO "role_permissions" VALUES ('d562c5b6-8e59-418a-9a4c-7f2cfedc1a86','f
 -- sysadmin
 INSERT INTO "role_permissions" VALUES ('ad048dda-a52a-4d4c-81eb-205b1f4723cb','aef02838-4b92-44e4-bf6a-dc8655b6844b','5fdf7895-3717-476d-8373-fc56a60350aa');
 INSERT INTO "role_permissions" VALUES ('6c096e21-359d-4ed9-942f-82dbb8427ea5','aef02838-4b92-44e4-bf6a-dc8655b6844b','c906813d-2268-4f94-9dfb-a467e4340cd5');
+-- fileuploader
+INSERT INTO "role_permissions" VALUES ('ca43e142-44b4-4843-8dea-340ae71ef5a9','e9c3921f-ccaa-4ad7-abfc-e8881f2440c5','86353fab-0b70-43f8-84bc-1c5d81a94f9e');
 
 -- root
 INSERT INTO "subject_roles" VALUES ('dbf31f00-d571-447e-bd9a-b6d57177d3c6','10886546-8af3-4bb1-b74e-c4d14cd47451','c0057bdd-b3ba-4e80-afb6-3cf5cb51ebce');
@@ -48,6 +52,27 @@ INSERT INTO "subject_roles" VALUES ('af7e691e-afcf-48f0-91b8-782f9687f078','4e5f
 INSERT INTO "subject_roles" VALUES ('10f7d87b-23cb-46e9-b1ae-a754b5557a4a','e9f00ee1-7687-4534-b938-f4c3e5367479','9b3e5f1b-c720-4242-a9d5-c243b68fc301');
 -- syseditor
 INSERT INTO "subject_roles" VALUES ('7110e1e4-21cb-4442-8780-3eeebe547295','4f04b79b-4ef3-49b6-a99f-bb187c4d7ca0','fe45a9e1-1fe6-4b9b-9c92-6f71559c5771');
+-- fileuploader projecteditor_baal
+INSERT INTO "subject_roles" VALUES ('d7abfb91-9e38-40a9-88df-e03780007b84','c7e4fb74-3232-446d-9129-ccd20222b9ed','e9c3921f-ccaa-4ad7-abfc-e8881f2440c5');
+
+INSERT INTO "subject" VALUES ('d3b429e6-f71e-4412-ad52-2d27d3a33c6d','tino','GjdIsQh1TZW1e0L+0s2AOyxY2jWuJd8zXatEV0rqLnQ=',current_date,'f05d2d8a-7dec-44dc-83b2-eb9e00b8fc03','mail@projecteditor.btu.de','root','root',1,'de-DE',current_date,null,null,null);
+INSERT INTO "subject" VALUES ('e66e47b7-470e-46c1-84e4-56472ebdeb66','frankh','2oBKRJhWHAsPFI+/IwUmXQoRb7L1TE68G4tQO7NHcIw=',current_date,'9fa5e53e-95ff-4419-a618-6dc2c30ea82f','mail@projecteditor.btu.de','root','root',1,'de-DE',current_date,null,null,null);
+INSERT INTO "subject" VALUES ('3cc1d241-e939-4538-8309-9649149e8236','franks','PXZApu1uKeoDrBkOd7ep2lRqZe+xG25H+2XwRnZAlNI=',current_date,'afdffe4e-b391-4ca1-a9bd-018c530754d9','mail@projecteditor.btu.de','root','root',1,'de-DE',current_date,null,null,null);
+INSERT INTO "subject" VALUES ('7f08131c-a6d9-4db1-8841-8a80a5066b6a','cornell','BbEuJjKEKCMfF3k/TOmealK3Tnu0Xz6rzfYaz1fi5Qw=',current_date,'208bd57f-01ee-49b0-bdcb-23fb4ad685ff','mail@projecteditor.btu.de','root','root',1,'de-DE',current_date,null,null,null);
+INSERT INTO "subject" VALUES ('44ca4b96-a599-4b6c-b291-d5bfd1994e3c','alex','TTgoFlU9IH90e8ybw02KfPtySzPObeDzxO+bm882Qs4=',current_date,'f3eaa8f9-5fc2-486b-9eab-e9a90d5281a6','mail@projecteditor.btu.de','root','root',1,'de-DE',current_date,null,null,null);
+INSERT INTO "subject" VALUES ('d8256603-f73e-449b-a1a0-5499b00b42f8','philipp','YxCGd2r7KG1xg/2Z1jbjkMdKdZ+V2UAjfozT4wNJZjc=',current_date,'3ffca723-6323-4181-84dd-d9f7e4fea5f2','mail@projecteditor.btu.de','root','root',1,'de-DE',current_date,null,null,null);
+INSERT INTO "subject" VALUES ('650b0144-b667-40a2-9fb9-0b545f218734','hanna','maLJKoTqv84aXU541IDtx0wWsZwZo6CkzEemM+7lpUM=',current_date,'dab1a46c-436f-4b80-a8ed-8672742a6aed','mail@projecteditor.btu.de','root','root',1,'de-DE',current_date,null,null,null);
+INSERT INTO "subject" VALUES ('5fbe0643-1c79-47ed-a8a9-777306eb7a7a','katja','kAnf6BDkh/f29eKTtvz5bkODvULH6Wu6mMDztNtJqBE=',current_date,'1f53110a-5ccb-4713-8bac-8e49379bc688','mail@projecteditor.btu.de','root','root',1,'de-DE',current_date,null,null,null);
+-- root
+INSERT INTO "subject_roles" VALUES ('18184349-9976-41e2-a20e-45c46fd358b4','d3b429e6-f71e-4412-ad52-2d27d3a33c6d','c0057bdd-b3ba-4e80-afb6-3cf5cb51ebce');
+INSERT INTO "subject_roles" VALUES ('e807ca15-bf98-4958-9351-0b7d0cd02858','e66e47b7-470e-46c1-84e4-56472ebdeb66','c0057bdd-b3ba-4e80-afb6-3cf5cb51ebce');
+INSERT INTO "subject_roles" VALUES ('04dd14aa-4a15-400d-85f7-1c662e17c186','3cc1d241-e939-4538-8309-9649149e8236','c0057bdd-b3ba-4e80-afb6-3cf5cb51ebce');
+INSERT INTO "subject_roles" VALUES ('c9e4b5d6-7253-4641-8440-081175d24be9','7f08131c-a6d9-4db1-8841-8a80a5066b6a','c0057bdd-b3ba-4e80-afb6-3cf5cb51ebce');
+INSERT INTO "subject_roles" VALUES ('b9602c30-44de-475a-b405-dfcc241ae340','44ca4b96-a599-4b6c-b291-d5bfd1994e3c','c0057bdd-b3ba-4e80-afb6-3cf5cb51ebce');
+INSERT INTO "subject_roles" VALUES ('cadb27d0-0307-4ea5-be2c-78186c456dc4','d8256603-f73e-449b-a1a0-5499b00b42f8','c0057bdd-b3ba-4e80-afb6-3cf5cb51ebce');
+INSERT INTO "subject_roles" VALUES ('67b63c57-d212-47e9-b8ec-7986d66a9312','650b0144-b667-40a2-9fb9-0b545f218734','c0057bdd-b3ba-4e80-afb6-3cf5cb51ebce');
+INSERT INTO "subject_roles" VALUES ('ab24a825-98bf-41d8-b898-5c7c52f8cc79','5fbe0643-1c79-47ed-a8a9-777306eb7a7a','c0057bdd-b3ba-4e80-afb6-3cf5cb51ebce');
+
 
 INSERT INTO "project_related_roles" VALUES ('0624533e-a8b0-4786-b590-885e414c88fe','projectadmin','Projektadministrator');
 INSERT INTO "project_related_roles" VALUES ('444749dc-d645-4ce8-9e01-c21d685ab690','projecteditor','Projektbearbeiter');
