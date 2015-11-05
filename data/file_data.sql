9c81b399d902db715672a91dbcccad5d6a716e9a
xa2kx <schulal2@b-tu.de>
2015-11-05 15:31:25 +0100

    more accepted mime types added

--- data/file_data.sql
+++ data/file_data.sql
@@ -5,4 +5,9 @@ SET client_encoding TO "UTF8";
 -------------------------- Insert default values -------------------------------
 --------------------------------------------------------------------------------
 INSERT INTO "supported_mime_types" VALUES ('a75f15e1-ec96-453a-80c6-2dd48c23dcba','image/*');
+INSERT INTO "supported_mime_types" VALUES ('1946ed10-eabe-4681-9c73-9c9cd3ddd69a','application/dxf');
 INSERT INTO "supported_mime_types" VALUES ('f9771aa9-9c1f-4221-bf6f-ace38092929e','application/pdf');
+INSERT INTO "supported_mime_types" VALUES ('0ca8f205-49db-40b9-a8d3-dd2611e50539','application/msword');
+INSERT INTO "supported_mime_types" VALUES ('4e9f7f50-3f05-4e1e-8812-fe190a785706','application/vnd.ms-excel');
+INSERT INTO "supported_mime_types" VALUES ('3d91af76-04cf-4647-9b63-c82f77b710c4','application/vnd.oasis.opendocument.spreadsheet');
+INSERT INTO "supported_mime_types" VALUES ('c5316048-8ffe-49bd-b847-1f58d5af8b79','application/vnd.oasis.opendocument.text');
